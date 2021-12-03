aff512a33bf552ba8e74e015fa91e1d45917c0e9
petrahlavinova <xhlavinova@g.stuba.sk>
2021-12-03 23:01:37 +0100

    final commit

--- backend/test.js
+++ backend/test.js
@@ -1,11 +1,11 @@
+const axios = require('axios')
 const assert = require('assert');
-const fetch = require('node-fetch');
 describe('Zahradkarske potreby - TESTY', function() {
 
   describe('Produkty', function() {
     it('ziskaj vsetky produkty', function() {
-        fetch('http://localhost:3030/products').then(d=>d.json()).then(d=>{
-            assert.equal(d.length > 0,true)
+        axios.get('http://localhost:3030/products').then(({data})=>{
+            assert.equal(data.length > 0,true)
           }).catch((error)=>{
             console.error(error);
           });
@@ -13,89 +13,70 @@ describe('Zahradkarske potreby - TESTY', function() {
   });
   describe('Objednavky', function() {
     before('vytvorenie objednavky pred testom', function(){
-        fetch('http://localhost:3030/payment',{
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                name:'Janko Testovaci',
-                email: 'janicko@gmail.com',
-                number: '0908888645',
-                street: 'Hlavna',
-                town: 'Testovacie Mesto',
-                zip: '98888',
-                cart: [{id:1,amount:1, price:8.5}]
-            })
-        })
+      axios.post('http://localhost:3030/payment',{
+        name:'Janko Testovaci',
+        email: 'janicko@gmail.com',
+        number: '0908888645',
+        street: 'Hlavna',
+        town: 'Testovacie Mesto',
+        zip: '98888',
+        cart: [{id:1,amount:1, price:8.5}]
+      })
     })
-    it('ziskaj vsetky objednavky', async function() {
-        const data = await fetch('http://localhost:3030/orders');
-        const parsed = await data.json();
-        assert.equal(parsed.length,true);
+    it('ziskaj vsetky objednavky', function() {
+        axios.get('http://localhost:3030/orders').then(({data})=>{
+          assert.equal(data.length > 0, true);
+        }).catch((error)=>{
+          console.error(error);
+        });
+        
     });
     it('vytvorenie objednavky',function(){
-        fetch('http://localhost:3030/payment',{
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                name:'Janko Testovaci',
-                email: 'janicko@gmail.com',
-                number: '0908888645',
-                street: 'Hlavna',
-                town: 'Testovacie Mesto',
-                zip: '98888',
-                cart: [{id:1,amount:1, price:8.5}]
-            })
-        }).then(d=>d.json()).then(data=>{
+        axios.post('http://localhost:3030/payment',{
+          name:'Janko Testovaci',
+          email: 'janicko@gmail.com',
+          number: '0908888645',
+          street: 'Hlavna',
+          town: 'Testovacie Mesto',
+          zip: '98888',
+          cart: [{id:1,amount:1, price:8.5}]
+        }).then(({data})=>{
             assert.equal(data.message,'Objednavka uspesne zaplatena');
         })
     })
-    it('zmena stavu objednavky', async function() {
-        const data = await fetch('http://localhost:3030/order/1',{
-            method: 'PUT',
-            headers: {
-                'content-type': 'application/json'
-            },
-            body: JSON.stringify({status:'delivered'})
+    it('zmena stavu objednavky', function() {
+        axios.put('http://localhost:3030/order/1',{status:'delivered'}).then(({data})=>{
+          assert.equal(data.message,'Úspešne zmenený stav objednávky');
+        }).catch((error)=>{
+          console.error(error);
         });
-        const parsed = await data.json();
-        assert.equal(parsed.message,'Úspešne zmenený stav objednávky');
     });
   });
   describe('Reklamy', function() {
     it('ziskaj vsetky reklamy', function() {
-        fetch('http://localhost:3030/adverts').then(d=>d.json()).then(d=>{
-            assert.equal(d.length > 0,true)
+        axios.get('http://localhost:3030/adverts').then(({data})=>{
+            assert.equal(data.length > 0,true)
           }).catch((error)=>{
             console.error(error);
           });
     });
     it('ziskaj náhodnu reklamu', function() {
-        fetch('http://localhost:3030/advert').then(d=>d.json()).then(d=>{
-            assert.equal(typeof d.image, 'string');
+        axios.get('http://localhost:3030/advert').then(({data})=>{
+            assert.equal(typeof data.image, 'string');
           }).catch((error)=>{
             console.error(error);
           });
     });
     it('kliknutie na reklamu', function() {
-        fetch('http://localhost:3030/advert/increment/1').then(d=>d.json()).then(d=>{
-            assert.equal(typeof d.link, 'string');
+        axios.get('http://localhost:3030/advert/increment/1').then(({data})=>{
+            assert.equal(typeof data.link, 'string');
           }).catch((error)=>{
             console.error(error);
           });
     });
     it('zmena presmerovania na reklamu', function() {
-        fetch('http://localhost:3030/advert/1',{
-            method: 'PUT',
-            headers: {
-                'content-type': 'application/json'
-            },
-            body: JSON.stringify({link:'https://ladypopular.sk/'})
-        }).then(d=>d.json()).then(d=>{
-            assert.equal(d.message, 'Úspešne zmenené presmerovanie reklamy');
+        axios.put('http://localhost:3030/advert/1',{link:'https://ladypopular.sk/'}).then(({data})=>{
+            assert.equal(data.message, 'Úspešne zmenené presmerovanie reklamy');
           }).catch((error)=>{
             console.error(error);
           });
